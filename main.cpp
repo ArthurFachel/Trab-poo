62c6928901dfc5a6196fae0d055a7808300826ea
Arthur Fachel <166940799+ArthurFachel@users.noreply.github.com>
2024-11-13 14:30:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-// main.cpp
 #include <iostream>
 #include "FileHandler.hpp"
 
 int main() {
-    string filename = "2020.csv"; // Nome do arquivo
+    string filename = "2020.csv"; 
     vector<DataItem> data = FileHandler::readDataFromFile(filename);
 
     FileHandler::printData(data);
